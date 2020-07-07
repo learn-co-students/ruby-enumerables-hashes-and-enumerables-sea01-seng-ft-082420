f92a0ec64dd1ab592c3a14869b1a89559d6311e0
Luke Hatcher <lukehatcher98@gmail.com>
2020-07-07 10:41:51 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |k, v|
+    if k == :suite_a && v.start_with?("A")
+      ans = v
+      return ans
+    end
+  end
 end
